b0468f17450928dc95c0e4c42163b66dfa1d89b4
Shashi Kumar <shashi4bs@gmail.com>
2022-02-12 17:41:15 +0530

    fix_weights_and_bias

--- b03/nn.py
+++ b03/nn.py
@@ -37,8 +37,8 @@ def buildNetwork(input_shape, hidden_layer_size, output_shape):
 			
 		bias.append(np.random.rand(hidden_layer_size[index]))
 	
-	bias.append(output_shape)
-	return weights, bias
+	bias.append(np.array(output_shape))
+	return np.array(weights), np.array(bias)
 	
 weights, bias = buildNetwork(4, [8, 6], 1)
 
@@ -59,7 +59,7 @@ def applyRelu(data):
 def feedForward(input_data, weights, bias):
 	feedData = input_data
 	activation = applyRelu
-	network = list()
+	network = [np.array(input_data)]
 	for w, b in zip(weights, bias):
 		z = np.dot(feedData, w) + b
 		output = activation(z)
@@ -74,28 +74,44 @@ print("1st Iteration : ", feedForward(input_data[0], weights, bias))
 def get_cost(output, index):
 	return (output_data[index] - output) ** 2
 
-def fix_weights_and_bias(cost, network, weights, bias):
-	'''
-		calculate gradient -> updated weights and bias
-	'''
-	print(weights[-1])
-	learning_rate = 0.01
-	new_weights, new_bias = list(), list()
-	delta_w = (cost) * network[-1] * applyDiffRelu(network[-2])
-	for index in range(network.shape[0] - 1, -1, -1):
-		weights[index] = weights[index] - (learning_rate * delta_w)
-		print("test: ", weights[index])
-		delta_w = delta_w * network[index] * applyDiffRelu(network[index - 1])					
-	return new_weights, new_bias 
+
+def fix_weights_and_bias(output, network, cost, weights, bias, lambda_w, lambda_b):
+	"""
+	calculate gradient and add it to lambda_w and lambda_b
+	"""
+	print("network: ", network)
+	print("weights: ", weights)
+	print("bias: ", bias)
+	print("lambda", lambda_b[-1])
+	z = (2 * (network[-1] - output)) * applyDiffRelu(network[-1])
+	lambda_b[-1] += z[0]
+	lambda_w[-1] += sum(z * network[-2])
+	z = z.reshape(1, -1)	
+	for index in range(2, weights.shape[0]):
+		z1 = np.dot(weights[-index + 1], z).T * applyDiffRelu(network[-index])
+		print(np.dot(weights[-index + 1], z).T)
+		print(applyDiffRelu(network[-index]))
+		print("z: ", z1) 
+		lambda_b[-index] += z1[0]
+		lambda_w[-index] += np.dot(z1, network[-index-1])
+		 
+	return lambda_w, lambda_b
 
 num_iterations = 1
 for _ in range(num_iterations):
 	cost = 0
 	network = None
+	lambda_w = [np.zeros(w.shape) for w in weights]
+	lambda_b = [np.zeros(b.shape) for b in bias]
 	for i in range(input_data.shape[0]): 
 		output, network = feedForward(input_data[i], weights, bias)
-		cost += get_cost(output[0], i)
+		cost = get_cost(output[0], i)
+		lambda_w, lambda_b = fix_weights_and_bias(output_data[i], network, cost, weights, bias, lambda_w, lambda_b)
+		break
+	'''
+	calculting gradient post every instance calculation
+	cost = cost / input_data.shape[0]
 	print("Cost : ", cost)
 	new_weights, new_bias = fix_weights_and_bias(cost, network, weights, bias)
 	weights, bias = new_weights, new_bias
-
+	'''
